b8a4b137e5471e2a4c41e8e2c4dcd721a4eaca97
Mostafa Suleiman <mustafasuleiman753@gmail.com>
2025-12-25 10:01:11 +0200

    fix script

--- rentora_backend/assign_all_vehicle_images.py
+++ rentora_backend/assign_all_vehicle_images.py
@@ -1,43 +1,43 @@
-import os
-import random
-from django.conf import settings
-from vehicles.models import Vehicle, VehicleImage
+# import os
+# import random
+# from django.conf import settings
+# from vehicles.models import Vehicle, VehicleImage
 
-IMAGES_DIR = os.path.join(settings.MEDIA_ROOT, "vehicles")
+# IMAGES_DIR = os.path.join(settings.MEDIA_ROOT, "vehicles")
 
-all_images = [
-    f for f in os.listdir(IMAGES_DIR)
-    if f.lower().endswith((".jpg", ".jpeg", ".png", ".webp"))
-]
+# all_images = [
+#     f for f in os.listdir(IMAGES_DIR)
+#     if f.lower().endswith((".jpg", ".jpeg", ".png", ".webp"))
+# ]
 
-if not all_images:
-    print("❌ No images found")
-    exit()
+# if not all_images:
+#     print(" No images found")
+#     exit()
 
-random.shuffle(all_images)
+# random.shuffle(all_images)
 
-vehicles = list(Vehicle.objects.all())
-image_index = 0
+# vehicles = list(Vehicle.objects.all())
+# image_index = 0
 
-# Clear old images
-VehicleImage.objects.all().delete()
-print("🧹 Old vehicle images cleared")
+# # Clear old images
+# VehicleImage.objects.all().delete()
+# print(" Old vehicle images cleared")
 
-for vehicle in vehicles:
-    images = []
+# for vehicle in vehicles:
+#     images = []
 
-    for i in range(3):
-        img = all_images[image_index % len(all_images)]
-        image_index += 1
+#     for i in range(3):
+#         img = all_images[image_index % len(all_images)]
+#         image_index += 1
 
-        images.append(
-            VehicleImage(
-                vehicle=vehicle,
-                image=f"vehicles/{img}",
-                is_primary=(i == 0),
-            )
-        )
+#         images.append(
+#             VehicleImage(
+#                 vehicle=vehicle,
+#                 image=f"vehicles/{img}",
+#                 is_primary=(i == 0),
+#             )
+#         )
 
-    VehicleImage.objects.bulk_create(images)
+#     VehicleImage.objects.bulk_create(images)
 
-print("🎉 Images assigned successfully to all vehicles")
+# print(" Images assigned successfully to all vehicles")
